5c82d0362451f7fc00b406f522ca052e509eca9d
Sru20 <sruthyks2409@gmail.com>
2024-12-05 22:47:08 +0530

    midified

--- sample.groovy
+++ sample.groovy
@@ -5,7 +5,7 @@ pipeline {
         stage("git"){
             steps {
                 echo "kjhjkhj"
-                
+                echo "hsii"
                 bat "git clone https://github.com/prabunsyam/Complete-Python-Bootcamp.git"
             }
         }
